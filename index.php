f934ccedcd6ce66f94cf6f4a3d28f9608a1e32ef
Mikhail Eremenko <m.eremenko@notamedia.com>
2024-07-03 20:45:03 +0300

    move log

--- index.php
+++ index.php
@@ -3,14 +3,6 @@ header('Content-Type: application/json');
 
 require_once 'vendor/autoload.php';
 
-if (!empty($_POST)) {
-    file_put_contents(
-        __DIR__.'/hooks.log',
-        json_encode($_POST,JSON_UNESCAPED_UNICODE).PHP_EOL,
-        FILE_APPEND
-    );
-}
-
 $ctrl = new \App\AmoCRMController();
 $result = $ctrl->hookHandler();
 
